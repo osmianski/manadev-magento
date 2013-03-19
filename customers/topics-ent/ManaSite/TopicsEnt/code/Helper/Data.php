bf2258eac4bdffe44ec0fe4d93bdc181d4c20043
Vladislav Osmianskij <vo@manadev.com>
2013-03-19 21:36:03 +0200

    Custom page templates for topics-ent.com

---
+++ customers/topics-ent/ManaSite/TopicsEnt/code/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+ * @category    Mana
+ * @package     ManaSite_TopicsEnt
+ * @copyright   Copyright (c) http://www.manadev.com
+ * @license     http://www.manadev.com/license  Proprietary License
+ */
+/**
+ * Generic helper functions for ManaSite_TopicsEnt module. This class is a must for any module even if empty.
+ * @author Mana Team
+ */
+class ManaSite_TopicsEnt_Helper_Data extends Mage_Core_Helper_Abstract {
+	// INSERT HERE: helper functions that should be available from any other place in the system
+}
