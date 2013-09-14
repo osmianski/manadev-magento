e3bd289b648e293d764833e758a643121f6470a5
nash <nash@manadev.com>
2013-09-14 16:18:57 +0300

    Filter update fix for UrlIndexer

--- extensions/Mana/Seo/code/Resource/AttributeUrlIndexer.php
+++ extensions/Mana/Seo/code/Resource/AttributeUrlIndexer.php
@@ -40,14 +40,37 @@ abstract class Mana_Seo_Resource_AttributeUrlIndexer extends Mana_Seo_Resource_U
             if ($event->getData('data_object')->getType() != 'category') {
                 $attributeId = $db->fetchOne($db->select()
                     ->from(array('a' => $this->getTable('eav/attribute')), 'attribute_id')
+                    ->joinInner(
+                        array('t' => $this->getTable('eav/entity_type')),
+                        "`t`.`entity_type_id` = `a`.`entity_type_id` AND `t`.`entity_type_code` = 'catalog_product'",
+                        null
+                    )
+                    ->joinInner(
+                        array('ca' => $this->getTable('catalog/eav_attribute')),
+                        "`ca`.`attribute_id` = `a`.`attribute_id`",
+                        null
+                    )
                     ->where('a.attribute_code = ?', $event->getData('data_object')->getCode()));
                 $event->addNewData('attribute_id', $attributeId);
+
+
+
             }
         }
         elseif ($event->getEntity() == 'mana_filters/filter2_store') {
             if ($attributeId = $db->fetchOne($db->select()
                 ->from(array('a' => $this->getTable('eav/attribute')), 'attribute_id')
                 ->joinInner(array('f' => $this->getTable('mana_filters/filter2')), "a.attribute_code = f.code", null)
+                ->joinInner(
+                    array('t' => $this->getTable('eav/entity_type')),
+                    "`t`.`entity_type_id` = `a`.`entity_type_id` AND `t`.`entity_type_code` = 'catalog_product'",
+                    null
+                )
+                ->joinInner(
+                    array('ca' => $this->getTable('catalog/eav_attribute')),
+                    "`ca`.`attribute_id` = `a`.`attribute_id`",
+                    null
+                )
                 ->where('f.id = ?', $event->getData('data_object')->getGlobalId()))
             ) {
                 $event->addNewData('attribute_id', $attributeId);
