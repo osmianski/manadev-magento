0d0bdd5f2e21464381a2646eaa484a293c9bcea3
Vladislav Osmianskij <vo@manadev.com>
2013-11-08 00:59:19 +0200

    phpstorm meta info

--- themes/default/manadev/js/local_manadev.js
+++ themes/default/manadev/js/local_manadev.js
@@ -145,9 +145,6 @@
 		}
 
 		/***************** CLIENT LOGIC FOR DOWNLOAD INITIATOR CONTROL **********************/
-		if ($.options("#download-initiator")) {
-			window.location.href = $.options("#download-initiator").fileUrl;
-		}
 
 		/***************** CLIENT LOGIC FOR FILE UPLOAD CONTROLS **********************/
 		$('input.file').change(function() {
@@ -314,4 +311,9 @@
 		});
         //endregion
 	});
+    $(window).on('load', function () {
+        if ($.options("#download-initiator")) {
+            window.location.href = $.options("#download-initiator").fileUrl;
+        }
+    });
 })(jQuery);
