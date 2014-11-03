505a576404ad9431008c8c86939479a8c40ba438
Vernard Alexis Luz <vern@manadev.com>
2014-11-03 15:45:05 +0800

    Fixed checking of selected tree item because sometimes url is not very accurate

--- extensions/Mana/Content/code/Model/Generator/Tree.php
+++ extensions/Mana/Content/code/Model/Generator/Tree.php
@@ -39,13 +39,12 @@ class Mana_Content_Model_Generator_Tree extends Mana_Menu_Model_Generator {
         $route = "mana_content/book/view";
         $url = Mage::getUrl($route, array('_use_rewrite' => true, 'id' => $id));
         $url = explode('?', $url)[0];
-        $currentUrl = Mage::helper('core/url')->getCurrentUrl();
 
         if(!is_array($filteredIds) || in_array($id, $filteredIds)) {
             $element->items->$xmlId->url = $url;
             $element->items->$xmlId->route = $route;
             $element->items->$xmlId->label = $book->getTitle();
-            $element->items->$xmlId->selected = $url == $currentUrl;
+            $element->items->$xmlId->selected = Mage::registry('current_book_page')->getId() == $id;
             $book->loadChildPages();
 
             foreach($book->getChildPages() as $record) {
