2db4fba6befae1e78a0d9f12d755f33f3f03d926
Vernard Alexis Luz <vern@manadev.com>
2014-11-17 13:29:12 +0800

    Fix bug having two canonical link if the referenced page is viewed and there is a search query

--- extensions/Mana/Content/code/controllers/BookController.php
+++ extensions/Mana/Content/code/controllers/BookController.php
@@ -40,7 +40,7 @@ class Mana_Content_BookController extends Mage_Core_Controller_Front_Action {
                 $head->setDescription($bookPage->getMetaDescription());
                 if($canonicalUrl = Mage::getResourceModel('mana_content/page_globalCustomSettings')->getReferencePageUrl($bookPage->getId())) {
                     $params = array('_nosid' => true, '_current' => true, '_m_escape' => '', '_use_rewrite' => true);
-                    $url = Mage::getUrl('*/*/*', $params);
+                    $url = explode('?', Mage::getUrl('*/*/*', $params))[0];;
                     $head->removeItem('link_rel', $url);
                     $head->addLinkRel('canonical', $canonicalUrl);
                 }
