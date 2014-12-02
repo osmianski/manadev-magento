a220910967d7786c927decf7d8b40498e2a8f983
Vernard Alexis Luz <vern@manadev.com>
2014-12-02 12:47:31 +0800

    Fix bug when existing record becomes child of new page

--- extensions/Mana/Content/code/controllers/Adminhtml/Mana/Content/BookController.php
+++ extensions/Mana/Content/code/controllers/Adminhtml/Mana/Content/BookController.php
@@ -99,9 +99,9 @@ class Mana_Content_Adminhtml_Mana_Content_BookController extends Mana_Admin_Cont
                             $tmpId = $fields['id']['value'];
                             unset($fields['id']);
                         }
-                        if (isset($fields['parent_id']['value']) && isset($newId[$fields['parent_id']['value']])) {
-                            $fields['parent_id']['value'] = $newId[$fields['parent_id']['value']];
-                        }
+                    }
+                    if (isset($fields['parent_id']['value']) && isset($newId[$fields['parent_id']['value']])) {
+                        $fields['parent_id']['value'] = $newId[$fields['parent_id']['value']];
                     }
                     if($action != "deleted") {
                         // data
