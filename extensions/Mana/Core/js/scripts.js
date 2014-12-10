103dfcfd80da964a1bf881b94689d46f44496abf
Vladislav Osmianskij <vo@manadev.com>
2014-12-10 13:57:00 +0200

    Fix: one customer site conatined window.enquire without register function

--- extensions/Mana/Core/js/scripts.js
+++ extensions/Mana/Core/js/scripts.js
@@ -1496,7 +1496,7 @@ Mana.require(['jquery'], function($) {
     };
     Mana.rwdIsMobile = false;
     $(function() {
-        if (window.enquire) {
+        if (window.enquire && window.enquire.register) {
             enquire.register('screen and (max-width: ' + bp.medium + 'px)', {
                 match: function () {
                     Mana.rwdIsMobile = true;
