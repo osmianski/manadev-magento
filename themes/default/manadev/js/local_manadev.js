a5d9331476248fa72ea02237f6ae6c250ae35d18
Vladislav Osmianskij <vo@manadev.com>
2015-08-07 07:43:28 +0300

    @manadev free download dialog fixed; tomenu link now works in dev environment too

--- themes/default/manadev/js/local_manadev.js
+++ themes/default/manadev/js/local_manadev.js
@@ -40,7 +40,7 @@
 						url: _downloadDialogOptions.guestUrl,
 						data: {},
 						success: function(data, status, request) {
-							if (typeof(data) === 'string') { data = $.parseJSON(data); }
+							if (typeof(data) === 'string' && data) { data = $.parseJSON(data); }
 							if (!data || !data.error) {
 								// close dialog and just open download page. Server should have been
 								// prepared to accept this download attempt
