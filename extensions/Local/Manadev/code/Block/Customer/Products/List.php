6ef94640d40cfb034cfa284dbd03c571ac824339
Vernard Alexis Luz <vern@manadev.com>
2016-09-07 01:49:21 +0100

    @license License Do not display license item with status "Not Available"

--- extensions/Local/Manadev/code/Block/Customer/Products/List.php
+++ extensions/Local/Manadev/code/Block/Customer/Products/List.php
@@ -28,25 +28,35 @@ class Local_Manadev_Block_Customer_Products_List extends Mage_Downloadable_Block
         'prolong_support_period' => 'Extend Support Period',
     );
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
-
-        /* @var $resource Mage_Core_Model_Resource */
-        $res = Mage::getSingleton('core/resource');
-
-        $select = $res->getConnection('read')->select();
-        $select
-            ->from(array('oi' => $res->getTableName('sales/order_item')),
-                "oi.item_id")
-            ->join(array('o' => $res->getTableName('sales/order')),
-                "`o`.`entity_id` = `oi`.`order_id` AND `o`.`status` = 'complete'", null);
-
-        /* @var $purchasedItems Mage_Downloadable_Model_Resource_Link_Purchased_Item_Collection */
-        $purchasedItems = $this->getItems();
-        $purchasedItems->getSelect()
-            ->where("`main_table`.`order_item_id` IN ({$select->__toString()})");
-
+        $session = Mage::getSingleton('customer/session');
+        $purchased = Mage::getResourceModel('downloadable/link_purchased_collection')
+            ->addFieldToFilter('customer_id', $session->getCustomerId())
+            ->addOrder('created_at', 'desc');
+        $this->setPurchased($purchased);
+        $purchasedIds = array();
+        foreach ($purchased as $_item) {
+            $purchasedIds[] = $_item->getId();
+        }
+        if (empty($purchasedIds)) {
+            $purchasedIds = array(null);
+        }
+        $purchasedItems = Mage::getResourceModel('downloadable/link_purchased_item_collection')
+            ->addFieldToFilter('purchased_id', array('in' => $purchasedIds))
+            ->addFieldToFilter('status',
+                array(
+                    'nin' => array(
+                        Mage_Downloadable_Model_Link_Purchased_Item::LINK_STATUS_PENDING_PAYMENT,
+                        Mage_Downloadable_Model_Link_Purchased_Item::LINK_STATUS_PAYMENT_REVIEW,
+                        // MANAdev Start:
+                        // Do not display license with status "Not Available"
+                        Local_Manadev_Model_Download_Status::M_LINK_STATUS_NOT_AVAILABLE,
+                        // MANAdev End:
+                    )
+                )
+            )
+            ->setOrder('item_id', 'desc');
         $this->setItems($purchasedItems);
     }
 
