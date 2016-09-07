947edff2d50537a648d84dbd1957b4aeee609d2a
Vernard Alexis Luz <vern@manadev.com>
2016-09-07 01:17:50 +0100

    @license Display store info if there is no registered URL in issued licenses grid

--- extensions/Local/Manadev/code/Block/Adminhtml/Renderer/RegisteredDomain.php
+++ extensions/Local/Manadev/code/Block/Adminhtml/Renderer/RegisteredDomain.php
@@ -8,19 +8,26 @@ class Local_Manadev_Block_Adminhtml_Renderer_RegisteredDomain extends Mage_Admin
 {
     public function render(Varien_Object $row) {
         $htmlId = $this->_getHtmlId() . microtime(true);
-        $html = "";
+        $registered_domain = $this->_getValue($row);
+        $toggle1_style = "";
+        $toggle2_style = "style='display:none'";
+        if(trim($registered_domain) == "") {
+            $toggle1_style = $toggle2_style;
+            $toggle2_style = "";
+        }
 
-        $html .= "<div>";
+        $html = "<div>";
 
-        $html .= "<div class='mana-toggle-1'>";
-        $html .= '  <input type="text" id="' . $htmlId . '" value="' . $this->_getValue($row) . '" class="input-text no-changes m-save-on-change m-registered-domain"/>';
+        $html .= "<div class='mana-toggle-1' $toggle1_style>";
+        $html .= '  <input type="text" id="' . $htmlId . '" value="' . $registered_domain . '" class="input-text no-changes m-save-on-change m-registered-domain"/>';
         $html .= "</div>";
-        $html .= "<div class='mana-toggle-2' style='display:none'>";
+
+        $html .= "<div class='mana-toggle-2' $toggle2_style>";
         $html .= '    <textarea row="2" class="m-store-info m-save-on-change">'.$row->getData('m_store_info').'</textarea>';
         $html .= "</div>";
 
-        $html .= "<a href='#' class='mana-toggle-1-trigger' style='display:none;'>" . Mage::helper('local_manadev')->__('Show Registered URL') . "</a>";
-        $html .= "<a href='#' class='mana-toggle-2-trigger'>" . Mage::helper('local_manadev')->__('Show Store Information') . "</a>";
+        $html .= "<a href='#' class='mana-toggle-1-trigger' $toggle2_style>" . Mage::helper('local_manadev')->__('Show Registered URL') . "</a>";
+        $html .= "<a href='#' class='mana-toggle-2-trigger' $toggle1_style>" . Mage::helper('local_manadev')->__('Show Store Information') . "</a>";
         $html .= "</div>";
 
         $dhCollection = $this->localHelper()->prepareDomainHistoryCollection($row->getData('item_id'));
