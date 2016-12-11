95206676fcb3fcfad9555d361a501f645cc10135
Vladislav Osmianskij <vo@manadev.com>
2016-12-11 11:51:14 +0200

    @manadev fix for adding support services to cart

--- extensions/Local/Manadev/code/Model/Cart.php
+++ extensions/Local/Manadev/code/Model/Cart.php
@@ -7,12 +7,6 @@
 class Local_Manadev_Model_Cart extends Mage_Checkout_Model_Cart
 {
     public function addProduct($productInfo, $requestInfo = null) {
-        if(
-            $productInfo->getSku() == Mage::getStoreConfig('local_manadev/support_services_sku') &&
-            !isset($requestInfo['m_license'])
-        ) {
-            Mage::throwException(Mage::helper('local_manadev')->__('Please select one from My Licenses.'));
-        }
         return parent::addProduct($productInfo, $requestInfo);
     }
 
