af3f2d8e8493a7b137d4fca7563167158031f2f2
Vladislav Osmianskij <vo@manadev.com>
2019-01-04 17:10:01 +0200

    New block showing all category brands. Setup:
        # app/design/frontend/base/default/template/catalog/category/view.phtml
    
        <div class="featured-brands service-brand-list">
            <?php echo $this->getChildHtml('option_featured') ?>
        </div>
    
        # app/design/frontend/rwd/default/layout/catalog.xml
    
        <block type="mana_attributepage/option_currentCategory" name="option_featured"
               template="mana/attributepage/option/images.phtml">
            <action method="setImageToShow"><column_group>product</column_group></action>
        </block>

--- extensions/Mana/AttributePage/code/Block/Option/CurrentCategory.php
+++ extensions/Mana/AttributePage/code/Block/Option/CurrentCategory.php
@@ -8,18 +8,10 @@ class Mana_AttributePage_Block_Option_CurrentCategory extends Mana_AttributePage
     public function getCollection() {
         /* @var Mana_AttributePage_Resource_OptionPage_Store_Collection $collection */
         $collection = $this->createOptionPageCollection()
-            ->addStoreFilter($this->getData('store_id'))
+            ->addStoreFilter(Mage::app()->getStore()->getId())
             ->setOrder('position', 'ASC');
 
         return $this->addCategoryFilterToCollection($collection);
-
-    }
-
-    /**
-     * @return Mana_AttributePage_Resource_OptionPage_Store_Collection|Object
-     */
-    public function createOptionPageCollection() {
-        return Mage::getResourceModel('mana_attributepage/optionPage_store_collection');
     }
 
     /**
@@ -27,6 +19,33 @@ class Mana_AttributePage_Block_Option_CurrentCategory extends Mana_AttributePage
      * @return Mana_AttributePage_Resource_OptionPage_Store_Collection
      */
     protected function addCategoryFilterToCollection($collection) {
+        $products = $this->getLayer()->getCurrentCategory()->getProductCollection();
+        $this->getLayer()->prepareProductCollection($products);
+
+        $db = $products->getConnection();
+        /* @var $res Mage_Core_Model_Resource */
+        $res = Mage::getSingleton('core/resource');
+
+        $select = $products->getSelect()
+            ->reset(Varien_Db_Select::COLUMNS)
+            ->distinct()
+            ->joinInner(array('eav' => $res->getTableName('catalog_product_index_eav')),
+                "`eav`.`entity_id` = `e`.`entity_id` AND " .
+                //$db->quoteInto("`eav`.`attribute_id` = ?", $attributeId) . " AND " .
+                $db->quoteInto("`eav`.`store_id` = ?", Mage::app()->getStore()->getId()), null)
+            ->joinInner(array('option_page' => $res->getTableName('m_option_page_global')),
+                "`option_page`.`option_id_0` = `eav`.`value`", 'id');
+
+        $optionPageGlobalIds = $db->fetchCol($select);
+        $collection->getSelect()->where("`main_table`.`option_page_global_id` IN (?)", $optionPageGlobalIds);
+        return $collection;
+    }
+
+    /**
+     * @return Mage_Catalog_Model_Layer|Mage_Core_Model_Abstract
+     */
+    protected function getLayer() {
+        return Mage::getSingleton('catalog/layer');
     }
 
 }
--- extensions/Mana/AttributePage/code/Block/Option/Images.php
+++ extensions/Mana/AttributePage/code/Block/Option/Images.php
@@ -4,16 +4,69 @@
  * @package     Mana_AttributePage
  * @copyright   Copyright (c) http://www.manadev.com
  * @license     http://www.manadev.com/license  Proprietary License
- */
-/**
- * @author Mana Team
  *
+ * @method string getImageToShow()
  */
 abstract class Mana_AttributePage_Block_Option_Images extends Mage_Core_Block_Template {
+    const PRODUCT_IMAGE = 'product';
+    const FEATURED_IMAGE = 'featured';
+    const BASE_IMAGE = 'base';
+
     /**
      * @return Mana_AttributePage_Resource_OptionPage_Store_Collection
      */
     abstract public function getCollection();
+
+    public function getImageUrlColumn() {
+        return $this->getImageToShow() == self::BASE_IMAGE ? 'image' : "{$this->getImageToShow()}_image";
+    }
+
+    public function getImageWidthColumn() {
+        return $this->getImageToShow() == self::BASE_IMAGE ? 'image_width' : "{$this->getImageToShow()}_image_width";
+    }
+    public function getImageHeightColumn() {
+        return $this->getImageToShow() == self::BASE_IMAGE ? 'image_height' : "{$this->getImageToShow()}_image_height";
+    }
+
+    /**
+     * @param Mana_AttributePage_Model_OptionPage_Store $optionPage
+     * @return string|null
+     */
+    public function getImageUrl($optionPage) {
+        $result = $optionPage->getData($this->getImageUrlColumn());
+        if (!$result) {
+            return null;
+        }
+
+        if (!$this->filesHelper()->shouldRenderImage($result)) {
+            return null;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param Mana_AttributePage_Model_OptionPage_Store $optionPage
+     * @return string|null
+     */
+    public function getImageWidth($optionPage) {
+        return $optionPage->getData($this->getImageWidthColumn());
+    }
+
+    /**
+     * @param Mana_AttributePage_Model_OptionPage_Store $optionPage
+     * @return string|null
+     */
+    public function getImageHeight($optionPage) {
+        return $optionPage->getData($this->getImageHeightColumn());
+    }
+
+    /**
+     * @return Mana_AttributePage_Resource_OptionPage_Store_Collection|Object
+     */
+    public function createOptionPageCollection() {
+        return Mage::getResourceModel('mana_attributepage/optionPage_store_collection');
+    }
 
     #region Dependencies
     /**
